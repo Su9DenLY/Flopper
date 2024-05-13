5b65b1a0714d3fa7df9c3eccff43837bdac9140d
suddenly <terebaev1234@gmail.com>
2024-05-14 00:34:14 +0300

    add project

---
+++ Flopper/Program.fs
@@ -0,0 +1,14 @@
+﻿open Flopper
+
+let input_file = System.Console.ReadLine()
+let input = System.IO.File.ReadAllText input_file
+
+let result = Parser.parse input
+
+match result with
+| Result.Ok res ->
+    // AST output:
+    // printfn $"{res}"
+    let exitCode = Interpreter.interpret res
+    printfn $"Program finished with {exitCode}"
+| Result.Error err -> printf $"{err}"
